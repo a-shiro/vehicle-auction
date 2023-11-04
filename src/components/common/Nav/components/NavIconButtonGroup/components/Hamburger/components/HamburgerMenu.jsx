e04f5896ec98cf40c089b62544cd5d7855073c2f
a-shiro <alekmitov20@gmail.com>
2023-11-04 11:20:39 +0200

    Refactor: Move menu list code to MenuOptions component

--- src/components/common/Nav/components/NavIconButtonGroup/components/Hamburger/components/HamburgerMenu.jsx
+++ src/components/common/Nav/components/NavIconButtonGroup/components/Hamburger/components/HamburgerMenu.jsx
@@ -1,14 +1,9 @@
-// Hooks
-import { useState } from "react";
+// Components
+import MenuOptions from "./components/MenuOptions";
 // CSS
 import styles from "./HamburgerMenu.module.css";
-import { Link } from "react-router-dom";
 
 function HamburgerMenu({ toggleMenu }) {
-  // TODO: Replace this with authentication system
-  const [hasUser, setHasUser] = useState(true);
-  //
-
   return (
     <div className={styles.menu}>
       <header className={styles.menuHeader}>
@@ -16,49 +11,9 @@ function HamburgerMenu({ toggleMenu }) {
         <button onClick={toggleMenu}>×</button>
       </header>
 
-      <div className={styles.menuBody}>
-        <ul className={styles.menuList}>
-          {hasUser && (
-            <>
-              <li>
-                <Link to="/account">Profile</Link>
-              </li>
-              <li>
-                <Link to="/watch-list">Watch List</Link>
-              </li>
-              <li>
-                <Link to="/account/listings">My Listings</Link>
-              </li>
-            </>
-          )}
-
-          <div>
-            <li>
-              <Link to="/">Live Auctions</Link>
-            </li>
-            <li>
-              <Link to="/past-auctions">Past Results</Link>
-            </li>
-            <li>
-              <Link to="about-us">What's Vroom Bids?</Link>
-            </li>
-            <li>
-              <button>Daily Email</button>
-            </li>
-          </div>
-
-          {hasUser && (
-            <>
-              <li>
-                <Link to="/account/settings">Settings</Link>
-              </li>
-              <li>
-                <button>Sign out</button>
-              </li>
-            </>
-          )}
-        </ul>
-      </div>
+      <ul className={styles.menuList}>
+        <MenuOptions />
+      </ul>
     </div>
   );
 }
