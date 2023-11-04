eaba31cb23dc0a5cb80d054522760d361ebb4581
a-shiro <alekmitov20@gmail.com>
2023-11-04 11:31:27 +0200

    Refactor: Clean code

--- src/components/common/Nav/components/NavIconButtonGroup/components/Hamburger/Hamburger.jsx
+++ src/components/common/Nav/components/NavIconButtonGroup/components/Hamburger/Hamburger.jsx
@@ -1,18 +1,16 @@
 // Hooks
 import { useState } from "react";
 // Components
+import { Fragment } from "react";
 import { ReactComponent as HamburgerIcon } from "../../../../../../../assets/svgs/hamburger-menu.svg";
+import HamburgerMenu from "./components/HamburgerMenu";
 // CSS
 import styles from "./Hamburger.module.css";
-import HamburgerMenu from "./components/HamburgerMenu";
 
 // FIXME: Switching from portrait to landscae on mobile enlarges the anchor tag font size (only when menu is active)
 
 function Hamburger() {
   const [isMenuActive, setIsMenuActive] = useState(false);
-  // TODO: Replace this with authentication system
-  const [hasUser, setHasUser] = useState(true);
-  //
 
   const toggleMenu = () => {
     // TODO: Disable body scrolling on open
@@ -20,19 +18,13 @@ function Hamburger() {
   };
 
   return (
-    <>
-      <li>
-        <button
-          onClick={toggleMenu}
-          className={styles.hamburgerBtn}
-          data-active-user={`${hasUser}`}
-        >
-          <HamburgerIcon />
-        </button>
-      </li>
+    <Fragment>
+      <button onClick={toggleMenu} className={styles.hamburgerBtn}>
+        <HamburgerIcon />
+      </button>
 
       {isMenuActive && <HamburgerMenu toggleMenu={toggleMenu} />}
-    </>
+    </Fragment>
   );
 }
 
