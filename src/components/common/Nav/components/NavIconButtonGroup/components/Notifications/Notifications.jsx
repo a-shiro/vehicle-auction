0453966e65e84a5a2839b80a37590b10511f82af
a-shiro <alekmitov20@gmail.com>
2023-11-06 08:55:26 +0200

    Add: Notifications component

---
+++ src/components/common/Nav/components/NavIconButtonGroup/components/Notifications/Notifications.jsx
@@ -0,0 +1,32 @@
+// Hooks
+import { useState } from "react";
+// Components
+import { Fragment } from "react";
+import { ReactComponent as NotificationIcon } from "../../../../../../../assets/svgs/notification-bell.svg";
+import NotificationMenu from "./components/NotificationMenu";
+// CSS
+import styles from "./Notifications.module.css";
+
+// FIXME: Switching from portrait to landscae on mobile enlarges the anchor tag font size (only when menu is active)
+
+function Notifications() {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    // TODO: Disable body scrolling on open
+    setIsMenuActive(!isMenuActive);
+  };
+
+  return (
+    <Fragment>
+      {/* TODO: Add notification bubble to indicate amount that user has */}
+      <button onClick={toggleMenu} className={styles.notificationsBtn}>
+        <NotificationIcon />
+      </button>
+
+      {isMenuActive && <NotificationMenu toggleMenu={toggleMenu} />}
+    </Fragment>
+  );
+}
+
+export default Notifications;
